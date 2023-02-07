374bd6829dafd957305eff51e67014813730be8e
mattmchugh4 <matthewmchugh4@gmail.com>
2023-02-06 20:33:33 -0700

    create customer table and main application page

--- src/controller/LoginPageController.java
+++ src/controller/LoginPageController.java
@@ -1,11 +1,22 @@
 package controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoginPageController implements Initializable {
+    public TableColumn customerIDColumn;
+    public TableColumn customeNameColumn;
+    public TableColumn customerAddressColumn;
+    public TableColumn customerZipColumn;
+    public TableColumn customerPhoneColumn;
+    public TableColumn customerDivisionColumn;
+    public TableView customerTable;
+
     /**
      *
      * @param url
@@ -14,5 +25,17 @@ public class LoginPageController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+
+    public void onDeleteCustomer(ActionEvent actionEvent) {
+    }
+
+    public void onUpdateCustomer(ActionEvent actionEvent) {
+    }
+
+    public void onAddCustomer(ActionEvent actionEvent) {
+    }
+
+    public void onAddAppointment(ActionEvent actionEvent) {
     }
 }
